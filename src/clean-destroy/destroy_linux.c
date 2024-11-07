eefdb0d5f3b72ee2dc7c443d585b1e670259f2cc
Osonseth <max.cesaria@gmail.com>
2024-11-07 04:30:38 +0100

    add: destroy func for img, fix: segfault when push ESC

--- src/clean-destroy/destroy_linux.c
+++ src/clean-destroy/destroy_linux.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   destroy_linux.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: junsan <junsan@student.42.fr>              +#+  +:+       +#+        */
+/*   By: max <max@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:18:19 by junsan            #+#    #+#             */
-/*   Updated: 2024/11/06 13:59:01 by junsan           ###   ########.fr       */
+/*   Updated: 2024/11/07 04:15:22 by max              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	destroy_mlx_specific(t_game *game)
 {
+	if (game->img.img)
+		mlx_destroy_image(game->mlx.ptr, game->img.img);
 	if (game->mlx.windows)
 		mlx_destroy_window(game->mlx.ptr, game->mlx.windows);
 	game->mlx.windows = NULL;
