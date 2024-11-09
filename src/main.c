9816629518853941660b3aa99d96a0cca5ec0c6d
Action2thefuture <dnstks0204@gmail.com>
2024-11-09 10:25:43 +0100

    Fix : Pressing X in a window closes the window but does not close the program

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: junsan <junsan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 17:13:49 by max               #+#    #+#             */
-/*   Updated: 2024/11/08 17:37:27 by junsan           ###   ########.fr       */
+/*   Updated: 2024/11/09 10:24:59 by junsan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static void	print_controlor(void)
 		free(line);
 	}
 	close(fd);
-	printf(RESET "\n");
+	printf(RESET "\n\n");
 	printf(PRINT_CYAN "\tW" RESET ": move forward\t");
 	printf(PRINT_CYAN "\tS" RESET ": move backward\n");
 	printf(PRINT_CYAN "\tA" RESET ": move left\t");
@@ -44,6 +44,13 @@ static int	game_loop(t_game *game)
 	return (0);
 }
 
+static int	close_window(t_game *game)
+{
+	clean_and_destroy_all(game, game->df);
+	exit(EXIT_SUCCESS);
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_description_file (desc_file) = {0};
@@ -56,6 +63,7 @@ int	main(int argc, char **argv)
 		return (1);
 	print_controlor();
 	mlx_hook(game.mlx.windows, 2, 1L << 0, key_press, &game);
+	mlx_hook(game.mlx.windows, 17, 0, close_window, &game);
 	mlx_loop_hook(game.mlx.ptr, game_loop, &game);
 	mlx_loop(game.mlx.ptr);
 	clean_and_destroy_all(&game, &desc_file);
