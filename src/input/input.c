e5e68e9a9e89fe5dd13accad75c3e0fd072a2a0f
Action2thefuture <dnstks0204@gmail.com>
2024-11-11 00:47:44 +0100

    Modify : Change keycode from E to R

--- src/input/input.c
+++ src/input/input.c
@@ -6,7 +6,7 @@
 /*   By: junsan <junsan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 08:11:31 by junsan            #+#    #+#             */
-/*   Updated: 2024/11/10 23:54:13 by junsan           ###   ########.fr       */
+/*   Updated: 2024/11/11 00:46:46 by junsan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	key_press(int keycode, t_game *game)
 		move_left(game);
 	else if (keycode == KEY_D)
 		move_right(game);
-	else if (BONUS && keycode == KEY_E)
+	else if (BONUS && keycode == KEY_R)
 		toggle_door(game);
 	else if (keycode == KEY_LEFT)
 		rotate_left(game);
